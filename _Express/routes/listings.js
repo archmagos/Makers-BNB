6f9921d3a8ab6a54a2cf12aef3c23bc131f89369
Fred <fredch87@outlook.com>
2018-07-18 18:25:08 +0100

    Initial express front-end set up

--- _Express/routes/listings.js
+++ _Express/routes/listings.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
       { "title":"Jolly Roger", "description":"This ship is baller", "price":2400},
       { "title":"Jolly David", "description":"This ship is awful", "price":1100}
     ]};
-  res.render('listings', { title: 'Test title', listings: mockJSON});
+  res.render('listings', { title: 'Test title', mockJSON: mockJSON});
 });
 
 module.exports = router;
