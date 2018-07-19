120f1f287fd164fdb5a12876ab2e6f4b47d09fb9
John Forster <j_forster@hotmail.co.uk>
2018-07-19 09:54:08 +0100

    JSON Debugging

--- _Express/public/javascripts/interface.js
+++ _Express/public/javascripts/interface.js
@@ -3,10 +3,5 @@ $(document).ready(function() {
   var listing = new Listing();
 
   $('#submit-details').on('click', function(event) {
-    event.preventDefault();
-    listing.setTitle($('#title').val());
-    listing.setPrice($('#price').val());
-    listing.setDescription($('#description').val());
-    console.log(listing);
   });
 });
--- _Express/routes/listings.js
+++ _Express/routes/listings.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var $ = require('jquery');
+
+// require("jsdom").env("", function(err, window) {
+// 	if (err) {
+// 		console.error(err);
+// 		return;
+// 	}
+//
+// 	$ = require("jquery")(window);
+// });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //listings = $.getJson('localhost:9292/listings');
-  mockJSON = {
-    "listings": [
-      { "title":"Jolly Roger", "description":"This ship is baller", "price":2400},
-      { "title":"Jolly David", "description":"This ship is awful", "price":1100}
-    ]};
-  res.render('listings', { title: 'Test title', mockJSON: mockJSON});
+  console.log($);
+  listings = $.getJSON('http://localhost:9292/listings')
+
+  // mockJSON = {
+  //   "listings": [
+  //     { "title":"Jolly Roger", "description":"This ship is baller", "price":2400},
+  //     { "title":"Jolly David", "description":"This ship is awful", "price":1100}
+  //   ]};
+  res.render('listings', { title: 'Test title', mockJSON: listings});
 });
 
 module.exports = router;
