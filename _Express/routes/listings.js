35405eb34dc8213a657b03c3599d523c4d85933a
Fred <fredch87@outlook.com>
2018-07-19 11:59:31 +0100

    Reads and interprets JSON files

--- _Express/routes/listings.js
+++ _Express/routes/listings.js
@@ -1,47 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var $ = require('jquery');
-var fetcher = function(url){
-  $.getJSON(url);
-}
 
-// require("jsdom").env("", function(err, window) {
-// 	if (err) {
-// 		console.error(err);
-// 		return;
-// 	}
-//
-// 	$ = require("jquery")(window);
-// });
-// function loadJSON(path)//, success, error)
-// {
-//     var xhr = new XMLHttpRequest();
-//     // xhr.onreadystatechange = function()
-//     // {
-//     //     if (xhr.readyState === XMLHttpRequest.DONE) {
-//     //         if (xhr.status === 200) {
-//     //             if (success)
-//     //                 success(JSON.parse(xhr.responseText));
-//     //         } else {
-//     //             if (error)
-//     //                 error(xhr);
-//     //         }
-//     //     }
-//     // };
-//     xhr.open("GET", path, true);
-//     xhr.send();
-// }
+var api = require('../public/javascripts/jsonRequest.js')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log($);
-  listings = fetcher('http://localhost:9292/listings');
 
-  // mockJSON = {
-  //   "listings": [
-  //     { "title":"Jolly Roger", "description":"This ship is baller", "price":2400},
-  //     { "title":"Jolly David", "description":"This ship is awful", "price":1100}
-  //   ]};
-  res.render('listings', { title: 'Test title', mockJSON: listings});
+  api.apiGet(function(data) {
+    res.render('listings', { title: 'Test title', mockJSON: data});
+  });
 });
 
 module.exports = router;
