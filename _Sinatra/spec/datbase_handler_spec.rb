7ecf07b3c63558b2e392ec616c67592cf8deb813
nabil far <nabil.far.contact@gmail.com>
2018-07-20 11:58:10 +0100

    add create and all method for the bookings

--- _Sinatra/spec/datbase_handler_spec.rb
+++ _Sinatra/spec/datbase_handler_spec.rb
@@ -30,6 +30,27 @@ describe DatabaseHandler do
     end
   end
 
+  describe '.create_booking' do
+    it'should create a booking' do
+      listing = { title: 'pass', price: '200', description: 'ship it now', image_url: 'http://helloamazone.com', rating: '5', capacity: '3200' }
+      DatabaseHandler.create(listing)
+
+      result = DatabaseConnection.query("SELECT id FROM listings").values[0][0]
+      booking = { name: 'John', listing_id: result, start_week: '44', end_week: '46' }
+      DatabaseHandler.create_booking(booking)
+
+      result = DatabaseConnection.query("SELECT * FROM bookings")
+      expect(result.map{|db_booking| db_booking['name']}).to include(booking[:name])
+      expect(result.map{|db_booking| db_booking['listing_id']}).to include(booking[:listing_id])
+      expect(result.map{|db_booking| db_booking['start_week']}).to include(booking[:start_week])
+      expect(result.map{|db_booking| db_booking['end_week']}).to include(booking[:end_week])
+    end
+  end
+
+
+
+
+
   describe '.all' do
     it'should returns all listings in a JSON ' do
       listing = { 'title': 'ship', 'price': '200', 'description': 'ship it now', 'image_url': 'http://helloamazone.com', 'rating': '5', 'capacity': '3200' }
@@ -41,6 +62,25 @@ describe DatabaseHandler do
       result = JSON.parse(DatabaseHandler.all)['listings']
       expect(result.map { |listing| listing['title'] }).to include 'ship'
       expect(result.map { |listing| listing['title'] }).to include 'ship2'
+    end
+  end
+
+  describe '.all_bookings' do
+    it'should returns all bookings in a JSON ' do
+
+      listing = { title: 'pass', price: '200', description: 'ship it now', image_url: 'http://helloamazone.com', rating: '5', capacity: '3200' }
+      DatabaseHandler.create(listing)
+
+      result = DatabaseConnection.query("SELECT id FROM listings").values[0][0]
+      booking = { name: 'John', listing_id: result, start_week: '44', end_week: '46' }
+      booking2 = { name: 'Nabil', listing_id: result, start_week: '47', end_week: '48' }
+      DatabaseHandler.create_booking(booking)
+      DatabaseHandler.create_booking(booking2)
+
+
+      result2 = JSON.parse(DatabaseHandler.all_bookings)['bookings']
+      expect(result2.map { |booking| booking['name'] }).to include 'John'
+      expect(result2.map { |booking| booking['name'] }).to include 'Nabil'
     end
   end
 
